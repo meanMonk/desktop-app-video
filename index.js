13c8a9e51d03bb81c33b86ae48fcd56ce5707cde
Sahil Kashetwar <sahilkashetwar24@gmail.com>
2022-03-26 01:19:30 +0530

    loading html docs in app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 
 app.on('ready', () => {
-  new BrowserWindow({})
+  const mainWindow = new BrowserWindow({}) 
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 })
