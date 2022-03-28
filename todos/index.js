142d74557c545263421662519f808be5bc879660
Sahil Kashetwar <sahilkashetwar24@gmail.com>
2022-03-28 11:15:56 +0530

    changes:  - adding menu  - adding empty menu incase of darwing  - checking the user operating system  - adding submenu as the quit and making it functional.  -

--- todos/index.js
+++ todos/index.js
@@ -1,3 +1,4 @@
+const { Menu } = require('electron');
 const electron = require('electron');
 
 const { app, BrowserWindow } = electron;
@@ -8,10 +9,46 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true, 
+      nodeIntegration: true,
       contextIsolation: false
     }
   });
-  
+
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+  // setup menu;
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+  
 });
+
+
+let menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add todo'
+      },
+      {
+        label: 'Quit',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Command+Q'
+          } else {
+            return 'Ctrl+!'
+          }
+        }),
+        click() {
+          app.quit();
+        }
+      },
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  menuTemplate = [ {
+    label: ''
+  }, ...menuTemplate ];
+}
